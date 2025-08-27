f1e57bd567d1995e45eab1c8012bf7f33f2e7a41
dstaggs-15 <daniel.staggs0215@gmail.com>
2025-08-27 00:10:31 -0400

    Update get_raw_data.py

--- pipeline/get_raw_data.py
+++ pipeline/get_raw_data.py
@@ -49,7 +49,7 @@ def get_raw_data():
     # --- 3. Download Roster/Player Information (Our "Player Master List") ---
     try:
         print("Downloading player roster information to create Player Master List...")
-        # CORRECTED: Using the definitive function name for modern library versions.
+        # Using the correct function name for the library version we are installing.
         roster_df = nfl.import_roster_data(years=YEARS)
         
         roster_df.sort_values(by='season', ascending=False, inplace=True)
