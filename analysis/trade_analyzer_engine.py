42657392e2ee5719489e011ad20c7a0dd6cb4134
dstaggs-15 <daniel.staggs0215@gmail.com>
2025-08-28 12:52:30 -0500

    Update trade_analyzer_engine.py

--- analysis/trade_analyzer_engine.py
+++ analysis/trade_analyzer_engine.py
@@ -33,14 +33,12 @@ def generate_trade_report():
         sys.exit(1)
 
     # --- MERGE AND PREPARE MASTER DATAFRAME ---
-    # Standardize all merges on 'player_display_name'
     df = pd.merge(df_vorp, df_ros, on='player_display_name', how='left')
     df = pd.merge(df, df_consistency[['player_display_name', 'std_dev']], on='player_display_name', how='left')
     df = pd.merge(df, df_start_score[['player_display_name', 'start_score']], on='player_display_name', how='left')
     
-    # DEFINITIVE FIX: Rename the column in the player master list BEFORE merging
-    df_players.rename(columns={'player_name': 'player_display_name'}, inplace=True)
-    df = pd.merge(df, df_players[['player_display_name', 'birth_date']], on='player_display_name', how='left')
+    # DEFINITIVE FIX: Explicitly merge using the different column names from each file
+    df = pd.merge(df, df_players[['player_name', 'birth_date']], left_on='player_display_name', right_on='player_name', how='left')
     
     df.rename(columns={'rank': 'ros_rank'}, inplace=True)
 
